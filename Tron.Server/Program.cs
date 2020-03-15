ac5fe38631b899f4bde89d4ab3fb0ccc3e4cf75c
Daniel Elek <elekdani95@gmail.com>
2020-03-15 20:04:32 +0100

    Generated protocol classes in separate library

--- Tron.Server/Program.cs
+++ Tron.Server/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tron.Protocol.Messages;
 
 namespace Tron.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
